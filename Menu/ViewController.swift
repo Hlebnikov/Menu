7e0ed23999394209080e062fd557dcbd83e06314
Александр Хлебников <khlebnikov@icerockdev.com>
2018-10-28 18:15:34 +0700

    Initial Commit

---
+++ Menu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Menu
+//
+//  Created by Александр Хлебников on 28.10.2018.
+//  Copyright © 2018 IceRockDev💎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
